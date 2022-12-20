d2f26238f447bad62f914dc8662450838b28c889
Rahmatshoh Rahmonov <rahmatshoh.rahmonov@mail.ru>
2022-12-20 22:48:10 +0300

    modified

--- main.js
+++ main.js
@@ -1,39 +1,30 @@
 // get Elements from html
 let menu = document.querySelectorAll(".menu li");
-let codeBox = document.querySelectorAll(".codeBox");
 let resultCodeFeild = document.getElementById("resultCodeFeild");
 let htmlCodeFeild = document.getElementById("htmlCodeFeild");
 let cssCodeFeild = document.getElementById("cssCodeFeild");
 let jsCodeFeild = document.getElementById("jsCodeFeild");
 let btnRun = document.getElementById("btnRun");
 
-// htmlCodeFeild.innerHTML=`<div id="test">Test HTML</div>`
-// cssCodeFeild.innerHTML=`#test { background-color: red; }`
+// add classes
 
 menu.forEach(function (listItem) {
   listItem.addEventListener("click", function () {
     let dataName = listItem.getAttribute("data-name");
     let addClassActive = listItem.classList.toggle("active");
-    let b = document.querySelector("#" + dataName + "CodeBox");
-    b.classList.toggle("d-none");
-    // console.log (dataName)
-    // console.log (b)
+    document.querySelector("#" + dataName + "CodeBox").classList.toggle("d-none");
   });
 });
 
+// generate to publish result
+
 function run() {
   btnRun.addEventListener("click", function () {
-    let textareaHTML = (resultCodeFeild.innerHTML = htmlCodeFeild.value += "<style>" + cssCodeFeild.value + "</style>");
-
-    //     $('#resultFrame').contents().find('html').html("<style>"+$
-// ('#css').val()+"</style>"+$("#html").val());  
-    
-
-
-    console.log(htmlCodeFeild.value);
-    // console.log(res);
-
-    resultCodeFeild.contentWindow.eval(jsCodeFeild.value);
+    let textareaHTML = (resultCodeFeild.innerHTML =
+      htmlCodeFeild.value.replace(/^\s+|\s+$/gm, "") +
+      "<style>" +
+      cssCodeFeild.value.replace(/^\s+|\s+$/gm, "") +
+      "</style>") + eval(jsCodeFeild.value);
   });
 }
 
