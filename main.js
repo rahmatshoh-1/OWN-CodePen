4328bb0b8602b6a4e1219db44ccc1328eb105098
rahmatshoh-1 <56288392+rahmatshoh-1@users.noreply.github.com>
2022-12-20 19:47:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,32 +7,18 @@ let cssCodeFeild = document.getElementById("cssCodeFeild");
 let jsCodeFeild = document.getElementById("jsCodeFeild");
 let btnRun = document.getElementById("btnRun");
 
-// htmlCodeFeild.innerHTML=`<div id="test">Test HTML</div>`
-// cssCodeFeild.innerHTML=`#test { background-color: red; }`
-
 menu.forEach(function (listItem) {
   listItem.addEventListener("click", function () {
     let dataName = listItem.getAttribute("data-name");
     let addClassActive = listItem.classList.toggle("active");
     let b = document.querySelector("#" + dataName + "CodeBox");
     b.classList.toggle("d-none");
-    // console.log (dataName)
-    // console.log (b)
   });
 });
 
 function run() {
   btnRun.addEventListener("click", function () {
     let textareaHTML = (resultCodeFeild.innerHTML = htmlCodeFeild.value += "<style>" + cssCodeFeild.value + "</style>");
-
-    //     $('#resultFrame').contents().find('html').html("<style>"+$
-// ('#css').val()+"</style>"+$("#html").val());  
-    
-
-
-    console.log(htmlCodeFeild.value);
-    // console.log(res);
-
     resultCodeFeild.contentWindow.eval(jsCodeFeild.value);
   });
 }
